b0cc5772430551313939158974c293d063699be7
iculmination <ovsiichuk.bohdan@gmail.com>
2025-07-03 18:13:25 +0000

    feat: axios instance, sass, zustand counter store

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // eslint-disable-next-line
+  webpackDevMiddleware: (config: any) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
